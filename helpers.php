a095111948ff7cb1e1937e16f1b8b6e81177bef4
AgustinMadygraf <agustin.mtto.madygraf>
2024-01-15 00:01:37 -0300

    jk

---
+++ helpers.php
@@ -0,0 +1,18 @@
+<?php
+function obtenerNombreCentroCosto($codigo) {
+    $nombresCentroCosto = [
+        '1' => 'Maquina de bolsas',
+        '2' => 'Boletas y folletería',
+        '3' => 'Logistica',
+        '4' => 'Administración',
+        '5' => 'Club',
+        '6' => 'Mantenimiento',
+        '7' => 'Comedor',
+        '8' => 'Guardia',
+        '9' => 'Sistemas',
+        '10' => 'Enfermería',
+    ];
+
+    return isset($nombresCentroCosto[$codigo]) ? $nombresCentroCosto[$codigo] : 'Desconocido';
+}
+?>
