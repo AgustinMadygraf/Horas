38829d005616265bdc81dea030b67041fdd982e5
AgustinMadygraf <agustin.mtto.madygraf>
2024-01-18 10:20:22 -0300

    Add jQuery and jQuery DataTables to the registro_horas_template.php file. (generated by blackbox.ai )

--- templates/registro_horas_template.php
+++ templates/registro_horas_template.php
@@ -1,14 +1,22 @@
-<?php // registro_horas_template.php ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Registro de Horas</title>
     <link rel="stylesheet" type="text/css" href="CSS/header.css">
     <link rel="stylesheet" type="text/css" href="CSS/table_styles.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 </head>
 <body>
     <?php if ($resultado->num_rows > 0): ?>
         <table border='1'>
+            <script>
+                $(document).ready( function () {
+                    $('table').DataTable();
+                });
+            </script>
+
             <tr>
                 <th>Legajo</th>
                 <th>Fecha</th>
@@ -19,13 +27,14 @@
             </tr>
             <?php while($fila = $resultado->fetch_assoc()): ?>
                 <?php 
+                $fechaObj = DateTime::createFromFormat('Y-m-d', $fila["fecha"]);
+                $fechaFormateada = $fechaObj ? $fechaObj->format('d/m/Y') : 'Fecha inválida';
                 $dia = date('l', strtotime($fila["fecha"]));
                 $diaEnEspañol = traducirDia($dia);
                 ?>
                 <tr>
-                <tr>
                     <td><?= $fila["legajo"] ?></td>
-                    <td><?= $fila["fecha"] ?></td>
+                    <td><?= $fechaFormateada ?></td>
                     <td><?= $diaEnEspañol ?></td>
                     <td><?= $fila["horas_trabajadas"] ?></td>
                     <td><?= obtenerNombreCentroCosto($fila["centro_costo"]) ?></td>
