c3b158d74a1b2eb79a353f8d6db4166af7391245
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2024-02-23 14:54:44 -0300

    Format dates in centro_costo.php (generated by blackbox.ai )

--- centro_costo.php
+++ centro_costo.php
@@ -1,28 +1,53 @@
 <!--centro_costo.php-->
 <br><br>
 <?php
+setlocale(LC_TIME, 'es_ES', 'Spanish_Spain', 'Spanish');
+
 $fecha_inicio = isset($_GET['fecha_inicio']) && !empty($_GET['fecha_inicio']) ? $_GET['fecha_inicio'] : '2023-11-01';
 $fecha_fin = isset($_GET['fecha_fin']) && !empty($_GET['fecha_fin']) ? $_GET['fecha_fin'] : '2023-11-20';
 $fecha_inicio_formato = date("d/m/Y", strtotime($fecha_inicio));
 $fecha_fin_formato = date("d/m/Y", strtotime($fecha_fin));
 $intervalo_dias = $fecha_fin - $fecha_inicio; //ayuda acá
-?>
-<form method="GET" action="centro_costo.php">
-    Fecha inicio: <input type="date" name="fecha_inicio" value="<?php echo $fecha_inicio; ?>">
-    Fecha fin: <input type="date" name="fecha_fin" value="<?php echo $fecha_fin; ?>">
-    <input type="submit" value="Filtrar">
-</form>
-
-<?php
 // Creación de objetos DateTime para las fechas de inicio y fin
 $fechaInicioObj = new DateTime($fecha_inicio);
 $fechaFinObj = new DateTime($fecha_fin);
-
+function formatearFechaCompleta($fecha) {
+    $formatter = new IntlDateFormatter('es_ES', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+    $fechaDateTime = new DateTime($fecha);
+    return $formatter->format($fechaDateTime);
+}
 // Calculando la diferencia entre las fechas
 $intervalo = $fechaInicioObj->diff($fechaFinObj);
-
+$fecha_inicio_formato_completo = formatearFechaCompleta($fecha_inicio);
+$fecha_fin_formato_completo = formatearFechaCompleta($fecha_fin);
 // Extrayendo el número total de días del intervalo
 $intervalo_dias = $intervalo->days;
+?>
+<br>
+<form method="GET" action="centro_costo.php">
+    <table>    
+        <tr>
+            <td>Fecha inicio:</td>
+            <td><input type="date" name="fecha_inicio" value="<?php echo htmlspecialchars($fecha_inicio); ?>"></td>
+            <td><?php echo htmlspecialchars($fecha_inicio_formato); ?></td>
+            <td><?php echo htmlspecialchars($fecha_inicio_formato_completo); ?></td>
+        </tr>
+        <tr>
+            <td>Fecha fin:</td>
+            <td><input type="date" name="fecha_fin" value="<?php echo htmlspecialchars($fecha_fin); ?>"></td>
+            <td><?php echo htmlspecialchars($fecha_fin_formato); ?></td>
+            <td><?php echo htmlspecialchars($fecha_fin_formato_completo); ?></td>
+        </tr>
+        <tr>
+            <td colspan="4"><input type="submit" value="Filtrar"></td>
+        </tr>
+    </table>
+</form>
+
+
+
+<?php
+
 
 echo "Cantidad de días seleccionados: " . $intervalo_dias . "<br><br>";
 
@@ -84,7 +109,7 @@ include 'includes/centro_costo_table.php';
 $fecha_inicio_formato = date("d/m/Y", strtotime($fecha_inicio));
 $fecha_fin_formato = date("d/m/Y", strtotime($fecha_fin));
 
-echo '<br><br><br> Datos desde "'.$fecha_inicio_formato.'" hasta "'.$fecha_fin_formato.'" <br>';
-echo "SQL = <br>" . htmlspecialchars($sql);
+//echo '<br><br><br> Datos desde "'.$fecha_inicio_formato.'" hasta "'.$fecha_fin_formato.'" <br>';
+//echo "SQL = <br>" . htmlspecialchars($sql);
 
 ?>
